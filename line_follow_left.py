e13f0c23f91f847534fcc7cebe5f60ce4c93a648
Christian Ermann <christianermann@gmail.com>
2017-02-22 21:44:35 -0600

    Beginning implementation of turn nodes.

--- line_follow_left.py
+++ line_follow_left.py
@@ -10,57 +10,65 @@ RED = 5
 WHITE = 6
 BROWN = 7
 
+LEFT = 90
+
 
 # Integer value between 0 and 1000 that limits the speed of the motors.
 max_speed = 180
 
-# Integer value between 0 and 100 that the robot tries to get the color sensor (in reflection mode) to return.
-# 35 is the approximate reflection value above the intersection of a white and black line.
-target_reflection = 35
-
+# 
 target_color = BLACK
 left_color = YELLOW
 right_color = WHITE
 
-# Float value greater than 0 that determines how severely the robot reacts to error.
-# Larger values cause larger corrections.
-error_scale = 1.15
+# 
 adjustment = 0.05
 error = 0
 
-# Boolean value (1 or -1) that determines whether the robot attempts to stay on the left or right side of a black line bordered
-# by a white line.
-# 1 = Left side of black line
-# -1 = Right side of black line
-direction = 1
+heading = 0
+
+last_color = 0
+
+turn_speed_reduction = 0.2
 
 
 # Initializes color sensor and ensures it is connected.
 cl = ColorSensor()
-assert cl.connected, "Connect color sensor"
+assert cl.connected, "Connect color sensor."
+
+# Initializes gyro sensor and ensures it is connected.
+gr = GyroSensor()
+assert gr.connected, "Connect gyro sensor."
 
 # Initializes infrared sensor and ensures it is connected.
 ir = InfraredSensor()
-assert ir.connected, "Connect IR sensor"
+assert ir.connected, "Connect IR sensor."
 
 # Initializes touch sensor and ensures it is connected.
 ts = TouchSensor()
-assert ts.connected, "Connect touch sensor"
+assert ts.connected, "Connect touch sensor."
 
 # Initializes left and right motors and ensures they are connected.
 l_motor = LargeMotor(OUTPUT_B)
-assert l_motor.connected, "Connect left motor to port B"
+assert l_motor.connected, "Connect left motor to port B."
 
 r_motor = LargeMotor(OUTPUT_C)
-assert r_motor.connected, "Connect right motor to port C"
+assert r_motor.connected, "Connect right motor to port C."
 
 
-# Sets color sensor to measure reflection intensity.
+# Sets color sensor to return an integer value representative of the color its seeing.
 cl.mode = "COL-COLOR"
 
 # Sets infrared sensor to measure proximity on a scale of 0% - 100%.
 # 0% is equivalent to 0 cm and 100% is approximately 70 cm.
 ir.mode = "IR-PROX"
+
+
+#
+def calibrate_gyro():
+	gr.mode = "GYRO-RATE"
+	gr.mode = "GYRO-ANG"
+	time.sleep(4)
 
 
 # Runs the motors until stopped while also allowing easy adjustment of speed.
@@ -77,74 +85,117 @@ def stop_motors():
 
 # Changes the speed of the motors to make the robot follow a line.
 
-def drive():
+def follow_road():
 	global error
+	global last_color
 
 	current_color = cl.value()
 
-	if current_color == BLACK:
-		if error > 0:
+	if current_color == RED:
+		stop_motors()
+		handle_node(last_color)
+
+	else:
+		if current_color == BLACK:
+			if error > 0:
+				error -= adjustment
+			if error < 0:
+				error += adjustment	
+
+		elif current_color == YELLOW:
+			error += adjustment
+
+		elif current_color == WHITE:
 			error -= adjustment
-		if error < 0:
-			error += adjustment	
+		
+		if error > 1:
+			error = 1
+		elif error < -1:
+			error = -1
 
-	elif current_color == YELLOW:
-		error += adjustment
+		left_motor_speed = (-1 * max_speed * error) + max_speed
+		right_motor_speed = (max_speed * error) + max_speed
 
-	elif current_color == WHITE:
-		error -= adjustment
+		if left_motor_speed > max_speed:
+			left_motor_speed = max_speed
+		elif left_motor_speed < -max_speed:
+			left_motor_speed = -max_speed
+
+		if right_motor_speed > max_speed:
+			right_motor_speed = max_speed
+		elif right_motor_speed < -max_speed:
+			right_motor_speed = -max_speed
+
+		l_motor.speed_sp = left_motor_speed
+		r_motor.speed_sp = right_motor_speed
+
+		run_motors()
+
+		last_color = current_color
+
+
+def handle_node(last_color):
+	locate_node_center(last_color)
+	turn_direction = get_directions()
+	turn(turn_direction)
+	exit_node()
+
+def get_directions():
+	return(LEFT)
+
+def locate_node_center(last_color):
+	while current_color != BLACK:
+
+		if last_color == BLACK:
+			l_motor.speed_sp = 30
+			r_motor.speed_sp = 30
+
+		run_motors()
+
+		current_color = cl.value()
+
+	stop_motors()
+
+def turn(turn_direction):
+	global heading
+
+	heading += turn_direction
+
+	angle = gr.value()
+
+	while angle != heading:
+		if angle < heading:
+			l_motor.speed_sp = -max_speed * turn_speed_reduction
+			r_motor.speed_sp = max_speed * turn_speed_reduction
 	
-	if error > 1:
-		error = 1
-	elif error < -1:
-		error = -1
+		if angle > heading:
+			l_motor.speed_sp = max_speed * turn_speed_reduction
+			r_motor.speed_sp = -max_speed * turn_speed_reduction
 
-	left_motor_speed = (-1 * max_speed * error) + max_speed
-	right_motor_speed = (max_speed * error) + max_speed
+		angle = gr.value()
 
-	if left_motor_speed > max_speed:
-		left_motor_speed = max_speed
-	elif left_motor_speed < -max_speed:
-		left_motor_speed = -max_speed
+	stop_motors()
 
-	if right_motor_speed > max_speed:
-		right_motor_speed = max_speed
-	elif right_motor_speed < -max_speed:
-		right_motor_speed = -max_speed
+def exit_node():
+	while current_color != RED:
+		l_motor.speed_sp = 30
+		r_motor.speed_sp = 30
 
-	l_motor.speed_sp = left_motor_speed
-	r_motor.speed_sp = right_motor_speed
+		run_motors()
 
+	while current_color != BLACK:
+		l_motor.speed_sp = 30
+		r_motor.speed_sp = 30
 
-def follow_line():
-	# Calculates error based on a target reflection value and actual reflection value.
-	# This value is multiplied by a float (error_scale) that allows adjustment of how severely the robot reacts to errors.
-	error = (target_reflection - cl.value()) * error_scale
+		run_motors()
 
-	
+	stop_motors()
 
-	# Modeled after ev3 steering function; y = (7.2 * Steering) + Power.
-	# "Steering" becomes "error" and "Power" becomes max_speed.
-	# The direction variable changes whether the sensor tracks the right or left side of a black line bordered by a white line.
-	l_speed = (-7.2 * error * direction) + max_speed
-	r_speed = (7.2 * error * direction) + max_speed
-
-	# Limits both motors to a maximum speed, similar to the original ev3 software.
-	if l_speed > max_speed:
-		l_speed = max_speed
-	if r_speed > max_speed:
-		r_speed = max_speed
-	
-	# Sets motors to run at their calculated speed.
-	l_motor.speed_sp = l_speed
-	r_motor.speed_sp = r_speed
-
+calibrate_gyro()
 
 # Runs only while the touch sensor is not activated and the infrared sensor doesn't detect anything within approximately 35 cm.
 while not ts.value(): #or ir.value() < 50):
-	#follow_line()
-	drive()
-	run_motors()
+	follow_road()
 
 # Stops the robot and notifies the user with a beep.
 stop_motors()
