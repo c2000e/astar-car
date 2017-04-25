7ed8753ba95b31ed2f1696d6aa7de256a55111e4
c2000e <christianermann@gmail.com>
2017-04-24 22:44:16 -0500

    Modified program for SACAD demo

--- sacad.py
+++ sacad.py
@@ -10,12 +10,42 @@ assert r_motor.connected, "Connect right motor to port C."
 btn = Button()
 
 while not btn.any():
-	l_motor.run_timed(time_sp = 4000, speed_sp = 360)
-	r_motor.run_timed(time_sp = 4000, speed_sp = 100)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	r_motor.run_timed(time_sp = 1000, speed_sp = 100)
 
-	sleep(1)
+	sleep(1000)
 
-	r_motor.run_timed(time_sp = 4000, speed_sp = 360)
-	l_motor.run_timed(time_sp = 4000, speed_sp = 100)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	r_motor.run_timed(time_sp = 1000, speed_sp = 100)
 
-	sleep(1)
+	sleep(1000)
+
+	l_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	r_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
+
+	l_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	r_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
+
+	r_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
+
+	r_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
+
+	r_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
+
+	r_motor.run_timed(time_sp = 1000, speed_sp = 360)
+	l_motor.run_timed(time_sp = 1000, speed_sp = 100)
+
+	sleep(1000)
