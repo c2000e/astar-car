8496ea880cf7f403b3838cdd703309d4d7897d41
c2000e <christianermann@gmail.com>
2017-04-26 11:09:26 -0500

    Modified program for SACAD demo

--- sacad.py
+++ sacad.py
@@ -8,14 +8,9 @@ assert l_motor.connected, "Connect left motor to port B."
 r_motor = LargeMotor(OUTPUT_C)
 assert r_motor.connected, "Connect right motor to port C."
 
-rc = RemoteControl()
-assert rc.connected, "Can't detect remote control."
-
 btn = Button()
 
-rc.on_red_up = True
-
-while not (btn.any() or rc.on_red_up):
+while not btn.any():
 	l_motor.run_timed(time_sp = 4000, speed_sp = 480)
 	r_motor.run_timed(time_sp = 4000, speed_sp = 100)
 	r_motor.wait_while("running")
@@ -23,5 +18,3 @@ while not (btn.any() or rc.on_red_up):
 	r_motor.run_timed(time_sp = 4000, speed_sp = 480)
 	l_motor.run_timed(time_sp = 4000, speed_sp = 100)
 	l_motor.wait_while("running")
-
-	rc.process()
